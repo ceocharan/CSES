6fb6ebc7b069a02010b7eb34690ae63108f268e2
Sricharan Peddinti <sricharan_peddinti@epam.com>
2026-06-13 17:20:23 +0530

    IncreasingArray solution

--- IncreasingArray.java
+++ IncreasingArray.java
@@ -4,8 +4,18 @@ class IncreasingArray{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr=new int[n];
-        int prev=-1;
+        long prev=sc.nextInt();
+        long res=0,v=0;
+        for(int i=1;i<n;i++){
+            v=sc.nextInt();
+            if(prev>v){
+                res+=prev-v;
+            }
+            else{
+                prev=v;
+            }
+        }
+        System.out.println(res);
 
     }
 }
