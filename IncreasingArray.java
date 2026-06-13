6cc36c19749ad090f03eadb76559d64414a6ceaa
Sricharan Peddinti <sricharan_peddinti@epam.com>
2026-06-13 17:09:58 +0530

    first problems

---
+++ IncreasingArray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class IncreasingArray{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int prev=-1;
+
+    }
+}
---
+++ WeirdAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class WeirdAlgorithm {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextInt();
+        while(n>1){
+            System.out.print(n+" ");
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                n*=3;
+                n++;
+            }
+        }
+        System.out.println(n);
+    }
+}
